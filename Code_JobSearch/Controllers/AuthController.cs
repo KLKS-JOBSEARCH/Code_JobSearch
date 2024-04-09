9a4c2b2a1acf0b8b98bcd52f1b5af939c2a07802
Nhựt Phi <phi211102@gmail.com>
2024-04-09 13:41:31 +0700

    update UI

---
+++ Code_JobSearch/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Code_JobSearch.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        public ActionResult Dangnhap()
+        {
+            return View();
+        }
+    }
+}
