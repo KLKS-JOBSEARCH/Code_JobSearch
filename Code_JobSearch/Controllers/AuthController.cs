d362352efe6090b515b86d10ff1985434cbe9a2f
Nhựt Phi <phi211102@gmail.com>
2024-04-15 07:16:34 +0700

    Regitster for employer

--- Code_JobSearch/Controllers/AuthController.cs
+++ Code_JobSearch/Controllers/AuthController.cs
@@ -17,5 +17,9 @@ namespace Code_JobSearch.Controllers
         {
             return View();
         }
+        public ActionResult Employer_Register()
+        {
+            return View();
+        }
     }
 }
