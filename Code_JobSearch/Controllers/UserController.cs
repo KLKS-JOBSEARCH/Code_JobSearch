c46ceed86e093cc6cbfc75b221fb8a22ea230e3b
Nhựt Phi <phi211102@gmail.com>
2024-05-08 22:31:01 +0700

    Update: Cập nhật giao điện form góp ý

--- Code_JobSearch/Controllers/UserController.cs
+++ Code_JobSearch/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Code_JobSearch.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,15 +9,74 @@ namespace Code_JobSearch.Controllers
 {
     public class UserController : Controller
     {
+        DatabaseDataContext db = new DatabaseDataContext();
         // GET: User
         public ActionResult CvUser()
         {
             return View();
         }
 
+
+
+        #region Góp ý
+        [HttpGet]
         public ActionResult GopY()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult GopY(GopY gy, FormCollection f)
+        {
+            var tieude = f["TieuDeGY"];
+            var noidung = f["NoiDungGY"];
+            var mucdohailong = f["MucDoHaiLong"];
+
+            ViewBag.HotenKH = tieude;
+            ViewBag.TenDN = noidung;
+
+            if (string.IsNullOrEmpty(tieude) ||
+              string.IsNullOrEmpty(noidung))
+            {
+                ViewData["Loi"] = "Vui lòng điền đầy đủ thông tin!";
+                return View();
+            }
+            // Kiểm tra radio button đã được chọn và có giá trị hợp lệ hay không
+            if (string.IsNullOrEmpty(mucdohailong) || !int.TryParse(mucdohailong, out int mucDo))
+            {
+                ViewData["Loi2"] = "Vui lòng chọn mức độ hài lòng!";
+                return View();
+            }
+
+            // Kiểm tra giá trị của radio button nằm trong khoảng từ 1 đến 5
+            if (mucDo < 1 || mucDo > 5)
+            {
+                ViewData["Loi2"] = "Mức độ hài lòng phải từ 1 đến 5!";
+                return View();
+            }
+            // Lấy thông tin ứng viên từ session
+            UngVien kh = Session["KH"] as UngVien;
+            if (kh != null)
+            {
+                gy.Id_UV = kh.Id_UV;
+
+                gy.TieuDe_GY = tieude;
+                gy.NoiDung_GY = noidung;
+                gy.MucDoHaiLong = Convert.ToInt16(mucdohailong); // Chuyển đổi giá trị string thành smallint
+
+                db.Gopies.InsertOnSubmit(gy);
+                db.SubmitChanges();
+
+                ViewBag.TB = "thành công!";
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // Xử lý khi không có phiên đăng nhập
+                ViewBag.TB = "Đăng nhập trước khi gửi góp ý!";
+                return RedirectToAction("Login", "Auth");
+            }
+        }
+        #endregion
+
     }
 }
